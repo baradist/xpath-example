0cd84d6fd3cf015207530092efd39b99ae7fbab8
Oleg Grigoryev <oleg_grigoryev@epam.com>
2017-09-27 23:22:31 +0300

    replace entity and mapper so as to reduce dependency of a subject domain

--- src/main/java/cf/baradist/xpath_example/XPathReader.java
+++ src/main/java/cf/baradist/xpath_example/XPathReader.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 
 import static javax.xml.xpath.XPathConstants.NODE;
 import static javax.xml.xpath.XPathConstants.NODESET;
@@ -25,32 +26,52 @@ import static javax.xml.xpath.XPathConstants.NODESET;
 public class XPathReader {
     private Document document;
     private XPath xPath;
-    private EmployeeNodeMapper employeeNodeMapper;
 
     public XPathReader(InputStream inputStream) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
         document = builder.parse(inputStream);
         xPath = XPathFactory.newInstance().newXPath();
-        employeeNodeMapper = new EmployeeNodeMapper();
     }
 
     public String getValue(String expression) throws XPathExpressionException {
         return xPath.compile(expression).evaluate(document);
     }
 
-    public Employee getNode(String expression) throws XPathExpressionException {
-        Node node = (Node) xPath.compile(expression).evaluate(document, NODE);
-
-        return employeeNodeMapper.nodToEmployee(node);
+    public Node getNode(String expression) throws XPathExpressionException {
+        return (Node) xPath.compile(expression).evaluate(document, NODE);
     }
 
     public List<String> getList(String expression) throws XPathExpressionException {
-        NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(document, NODESET);
+        NodeList nodeList = getNodeList(expression);
         List<String> result = new ArrayList<>();
         for (int i = 0; i < nodeList.getLength(); i++) {
             result.add(nodeList.item(i).getFirstChild().getNodeValue());
         }
         return result;
+    }
+
+    public NodeList getNodeList(String expression) throws XPathExpressionException {
+        return (NodeList) xPath.compile(expression).evaluate(document, NODESET);
+    }
+
+    public static void print(Node node) {
+        System.out.println(nodeToString(node));
+    }
+
+    public static String nodeToString(Node node) {
+        final StringJoiner joiner = new StringJoiner("\n");
+        if (null != node) {
+            NodeList nodeList = node.getChildNodes();
+            for (int i = 0; null != nodeList && i < nodeList.getLength(); i++) {
+                Node nod = nodeList.item(i);
+                if (nod.getNodeType() == Node.ELEMENT_NODE) {
+                    joiner.add((nodeList.item(i).getNodeName())
+                            + " : "
+                            + nod.getFirstChild().getNodeValue());
+                }
+            }
+        }
+        return joiner.toString();
     }
 }
